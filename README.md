a34e88f75a46767d2304fba81a2ad34b011fc69f
Gabriel Przytocki <gabrielhprzytocki@gmail.com>
2022-11-22 17:51:03 -0300

    Adding new video and script updates

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
-# object-tracking
+# Object Tracking
 🥎 Object tracking with Python and OpenCV
 
-# Videos
+# Link to Videos
 - Ping-pong 1: https://www.youtube.com/shorts/KYGj2xHDQAg
 - Pendulum: https://www.youtube.com/watch?v=JSclyAqbYcc
 - Basket 1: https://www.youtube.com/watch?v=GG-8c0aqtjo
@@ -11,3 +11,19 @@
 - Soccer ball: https://www.youtube.com/watch?v=01cm7H1QoJg&ab_channel=RafalKruk
 - Ping-pong 2: https://www.youtube.com/watch?v=gTsx5eW2eLU&ab_channel=LeslieWatters
 - Dominoes: https://www.youtube.com/watch?v=8H4ipvzGkBY&ab_channel=domino3842
+
+# Usage examples
+- Example 1: `basket.mp4`
+    ```bash
+    python tracking.py --file './input/basket.mp4' --area '(10000,50000)'
+    ```
+
+- Example 2: `soccer_ball.mp4`
+    ```bash
+    python tracking.py --file './input/soccer_ball.mp4' --area '(500, 10000)'
+    ```
+
+- Example 3: `bouncing_ball.mp4`
+    ```bash
+    python tracking.py --file './input/bouncing_ball.mp4' --area '(200, 1000)'
+    ```
--- Tracking.py
+++ Tracking.py
@@ -1,65 +1,111 @@
-import sys
+import argparse
 import cv2 as cv
 
-file_path = sys.argv[1]
+parser = argparse.ArgumentParser(
+    prog = 'object_tracker',
+    description = 'Tracing objects with OpenCV'
+)
+
+parser.add_argument('-f', '--file')
+parser.add_argument('-a', '--area')
+parser.add_argument('-t', '--tail', default=.7)    
+parser.add_argument('-c', '--contours', default=False)
+
+args = parser.parse_args()
+
+file_path = args.file
+tail_start_thresh = args.tail
+draw_contours = args.contours
+
+area_inf = float(args.area.split(',')[0][1:])
+area_sup = float(args.area.split(',')[1].strip()[0:-1])
+
 video = cv.VideoCapture(file_path)
 
 if (video.isOpened()== False):
     print('erro ao abrir o arquivo de video')
 
+# subtração do fundo das imagens
+subtractor = cv.bgsegm.createBackgroundSubtractorMOG()
+
+# centros dos contornos dos objetos
+# em movimento
 centers = []
 
-while(video.isOpened()):
-    # altura e largura do video
-    height = video.get(cv.CAP_PROP_FRAME_HEIGHT)
-    width = video.get(cv.CAP_PROP_FRAME_WIDTH)
-    ret, frame = video.read()
+if __name__ == '__main__':
+    while(video.isOpened()):
+        height = video.get(cv.CAP_PROP_FRAME_HEIGHT)
+        width = video.get(cv.CAP_PROP_FRAME_WIDTH)
 
-    if ret == True:
-        # time.sleep(0.05)
+        ret, frame = video.read()
 
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        blurred = cv.GaussianBlur(gray, (11, 11), 0)
+        if ret == True:
+            # time.sleep(0.05)
 
-        # basket = 50
-        # bouncing_ball = 120
-        thresh, bin = cv.threshold(blurred, 120, 255, cv.THRESH_BINARY_INV)
-
-        erode = cv.erode(bin, (5,5), iterations=2)
-        dilate = cv.dilate(erode, (5,5), iterations=2)
-
-        contours, img = cv.findContours(dilate, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-        # center = 0
-
-        # cv.drawContours(frame, contours, -1, (0,255,0), 3)
-
-        for cnt in contours:
-            area = cv.contourArea(cnt)
-
-            # basket = (8000, 12000)
-            # bouncing_ball = (50, 3000)
-            if 500 < area < 3000:
-                # cv.drawContours(frame, [cnt], -1, (0,255,0), 3)
-
-                M = cv.moments(cnt)
-                cX = int(M['m10'] / M['m00'])
-                cY = int(M['m01'] / M['m00'])
-                centers.append((cX, cY))
-
-                for center_x, center_y in centers[int(len(centers)*.3):-1]:
-                    cv.circle(frame, (center_x, center_y), 3, (0, 255, 0), -1)
-
-                # último círculo
-                cv.circle(frame, (cX, cY), 7, (0, 255, 0), -1)
+            gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+            blurred = cv.GaussianBlur(gray, (11, 11), 0)
             
-        cv.imshow('binary', dilate)
-        cv.imshow('original', frame)
+            # thresh, bin = cv.threshold(blurred, 120, 255, cv.THRESH_BINARY_INV)
+            img_bin = subtractor.apply(blurred)
 
-        if cv.waitKey(25) & 0xFF == ord('q'):
+            erode = cv.erode(img_bin, (5,5), iterations=2)
+            dilate = cv.dilate(erode, (5,5), iterations=2)
+
+            contours, img = cv.findContours(
+                dilate,
+                cv.RETR_TREE,
+                cv.CHAIN_APPROX_SIMPLE
+            )
+
+            for cnt in contours:
+                area = cv.contourArea(cnt)
+
+                if area_inf < area < area_sup:
+
+                    if draw_contours:
+                        cv.drawContours(frame, [cnt], -1, (0,255,0), 1)
+
+                    M = cv.moments(cnt)
+                    cX = int(M['m10'] / M['m00'])
+                    cY = int(M['m01'] / M['m00'])
+                    centers.append((cX, cY))
+
+                for i, (center_x, center_y) in \
+                    enumerate(centers[int(len(centers)*tail_start_thresh):-1]):
+                    
+                    # círculos da trajetória
+                    cv.circle(
+                        frame,
+                        (center_x, center_y),
+                        3,
+                        (0, 255, 0),
+                        -1
+                    )
+
+                    # linhas da trajetória
+                    for i in range(int(len(centers)*tail_start_thresh), len(centers)-1):
+                        center = centers[i]
+                        next_center = centers[i+1]
+
+                        cv.line(
+                            img=frame,
+                            pt1=(center[0], center[1]),
+                            pt2=(next_center[0], next_center[1]),
+                            color=(0, 255, 0),
+                            thickness=1
+                        )
+
+                    # último círculo
+                    cv.circle(frame, (cX, cY), 7, (0, 255, 0), -1)
+                
+            cv.imshow('binary', dilate)
+            cv.imshow('original', frame)
+
+            if cv.waitKey(25) & 0xFF == ord('q'):
+                break
+
+        else:
             break
 
-    else:
-        break
-
-video.release()
-cv.destroyAllWindows()
+    video.release()
+    cv.destroyAllWindows()
